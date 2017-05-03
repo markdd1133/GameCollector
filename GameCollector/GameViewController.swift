5f47a96d3d62f42e614ada03a0eb2aab44e6cd84
markdd1133 <shengchichen@Shengde-MacBook-Air.local>
2017-05-03 09:51:30 +0800

    It’s working

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -9,19 +9,31 @@
 import UIKit
 
 class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var addUpdate: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
     var imagePicker = UIImagePickerController()
+    var game:Game?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        if game != nil{
+            imageView.image = UIImage(data: game!.image as! Data)
+            titleTextField.text = game!.title
+            addUpdate.setTitle("Update", for: .normal)
+        }else{
+            deleteButton.isHidden = true
+        }
     }
     
     @IBAction func camera(_ sender: Any) {
-        
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func photos(_ sender: Any) {
@@ -30,19 +42,34 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         imageView.image = image
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func add(_ sender: UIButton) {
-        let appDel = UIApplication.shared.delegate as! AppDelegate
-        let context = appDel.persistentContainer.viewContext
-        let game = Game(context: context)
-        game.title = titleTextField.text
-        game.image = UIImagePNGRepresentation(imageView.image!)! as NSData
-        appDel.saveContext()
+        if game != nil{
+            game!.title = titleTextField.text
+            game!.image = UIImagePNGRepresentation(imageView.image!) as NSData?
+        }else{
+            let appDel = UIApplication.shared.delegate as! AppDelegate
+            let context = appDel.persistentContainer.viewContext
+            let game = Game(context: context)
+            game.title = titleTextField.text
+            game.image = UIImagePNGRepresentation(imageView.image!) as NSData?
+        }
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController?.popViewController(animated: true)
     }
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        let appDel = UIApplication.shared.delegate as! AppDelegate
+        let context = appDel.persistentContainer.viewContext
+        context.delete(game!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+    }
+    
     
 }
--- GameCollector/ViewController.swift
+++ GameCollector/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-
+    
     @IBOutlet weak var tableView: UITableView!
     var games:[Game] = []
     
@@ -41,11 +41,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let game = games[indexPath.row]
         cell.textLabel?.text = game.title
         cell.imageView?.image = UIImage(data: game.image as! Data)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let game = games[indexPath.row]
+        performSegue(withIdentifier: "gameSegue", sender: game)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! GameViewController
+        nextVC.game = sender as? Game
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            let appDel = UIApplication.shared.delegate as! AppDelegate
+            let context = appDel.persistentContainer.viewContext
+            context.delete(games[indexPath.row])
+            appDel.saveContext()
+            do{
+                games = try context.fetch(Game.fetchRequest())
+                tableView.reloadData()
+            }catch{
+                let err = error as NSError
+                print(err.debugDescription)
+            }
+        default:
+            return
+        }
     }
 }
 
